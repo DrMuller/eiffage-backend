6bd52ebe5285471c8ab3774cac7b936bc9d128f8
drmuller <vincent.mullier@gmail.com>
2025-12-09 11:51:52 +0100

    open rh webapp to managers

--- src/auth/routes/user.admin.routes.ts
+++ src/auth/routes/user.admin.routes.ts
@@ -4,10 +4,10 @@ import { deleteUser, getManagers, getAllUsers, updateUserById, searchUsersHandle
 import { createUserWithoutPasswordHandler } from "../controller/auth.controller";
 
 const router = express.Router();
-router.get("/users/", [jwtMiddleware(['ADMIN'])], getAllUsers);
-router.get("/users/search", [jwtMiddleware(['ADMIN'])], searchUsersHandler);
-router.get("/users/managers", [jwtMiddleware(['ADMIN'])], getManagers);
-router.get("/users/:id", [jwtMiddleware(['ADMIN'])], getUserByIdHandler);
+router.get("/users/", [jwtMiddleware(['ADMIN', 'MANAGER'])], getAllUsers);
+router.get("/users/search", [jwtMiddleware(['ADMIN', 'MANAGER'])], searchUsersHandler);
+router.get("/users/managers", [jwtMiddleware(['ADMIN', 'MANAGER'])], getManagers);
+router.get("/users/:id", [jwtMiddleware(['ADMIN', 'MANAGER'])], getUserByIdHandler);
 router.post("/users", [jwtMiddleware(['ADMIN'])], createUserWithoutPasswordHandler);
 router.post("/users/:id/invite", [jwtMiddleware(['ADMIN'])], inviteUser);
 router.put("/users/:id", [jwtMiddleware(['ADMIN'])], updateUserById);
