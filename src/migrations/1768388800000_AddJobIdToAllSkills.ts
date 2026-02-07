6fd6a39b9ba32ce1ea2fdb70fe35a7c09052c3de
drmuller <vincent.mullier@gmail.com>
2026-02-07 18:23:48 +0100

    change job code

--- src/migrations/1768388800000_AddJobIdToAllSkills.ts
+++ src/migrations/1768388800000_AddJobIdToAllSkills.ts
@@ -3,11 +3,8 @@ import { MigrationInterface } from 'mongo-migrate-ts';
 
 export class AddJobIdToAllSkills1768388800000 implements MigrationInterface {
     public async up(db: Db): Promise<void | never> {
-        // Step 1: Find the job with code "emp_0678"
-        const job = await db.collection('job').findOne({ code: 'emp_0678' });
-        if (!job) throw new Error('Job with code "emp_0678" not found. Cannot proceed with migration.');
-        
-
+        const job = await db.collection('job').findOne({ code: 'Emp_0678' });
+        if (!job) throw new Error('Job with code "Emp_0678" not found. Cannot proceed with migration.');
         await db.collection('skill').updateMany({}, { $set: { jobId: job._id } });
         await db.collection('macro_skill').updateMany({}, { $set: { jobId: job._id } });
     }
